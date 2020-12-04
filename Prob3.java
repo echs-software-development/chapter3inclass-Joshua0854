23cc0b947e4cfb246d9af76b86a852f664e008b3
Joshua0854 <73349429+Joshua0854@users.noreply.github.com>
2020-12-04 14:49:18 +0000

    Prob 1-3

--- Prob3.java
+++ Prob3.java
@@ -1,10 +1,36 @@
 import java.util.Scanner;
-
+ 
 public class Prob3  {
-  public static void main(String[] args){
-    
-
-
-    
-  }
+ public static void main(String[] args){
+ Scanner scan = new scanner(system.in);
+ system.out.println("Please enter a number 0-4");
+ 
+ int grade = scan.nextInt();
+ 
+ if (grade == 4){
+   system.out.println("A");
+ }
+ 
+ else if (grade == 3){
+  system.out.println("B");
+ }
+ 
+ else if (grade == 2){
+  system.out.println("C");
+ }
+ 
+else (grade == 1){
+  system.out.println("D");
+}
+ 
+ 
+ else{
+   system.out.println("0");
+ }
+ 
+  
+ 
+ 
+  
+ }
 }
--- Prob4.java
+++ Prob4.java
@@ -1,10 +1,20 @@
 import java.util.Scanner;
-
+ 
 public class Prob4  {
-  public static void main(String[] args){
-    
-
-
-    
-  }
+ public static void main(String[] args){
+ 
+   Scanner scan = new Scanner(System.in);
+   system.out.println("What is the temperature");
+ 
+   double temp = scan.nextDouble();
+ 
+   if (temp <99|| > 102) {
+    system.out.println("Warning temp is too hot");
+   } 
+ 
+   else {
+    system.out.println("Temp is OK");
+   }
+  
+ }
 }
